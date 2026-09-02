be729d3fac64678c86a1aa550589ef7858f65c53
Katllyn Rezende <Katllynrezende@gmail.com>
2026-09-02 19:54:51 -0300

    Alterações na classe Repositorio

--- siga-solid/src/siga/RelatorioRepositorio.java
+++ siga-solid/src/siga/RelatorioRepositorio.java
@@ -2,7 +2,6 @@ package siga;
 
 public class RelatorioRepositorio {
 
-    // Responsabilidade (b): PERSISTÊNCIA
     public void salvarEmArquivo(String conteudo, String caminho) {
         System.out.println("[disco] Gravando relatório em: " + caminho);
         System.out.println(conteudo);
